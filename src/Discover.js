8358b76e36e804cff3a059a6bfa499a2692e865a
Kevin Alexander <kalex.bica@gmail.com>
2020-03-14 20:58:30 -0600

    create layout for discover page and Gamecard component

--- src/Discover.js
+++ src/Discover.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './Discover.css';
 import GameCard from './GameCard';
-import OW from './ow.jpg';
-import R6 from './r6.jpg'
-import dota from './dota.jpg'
+import OW from './img/ow.jpg';
+import R6 from './img/r6.jpg'
+import dota from './img/dota.jpg'
+import R6D from './img/r6dark.jpg';
+import apex from './img/apex.jpeg';
+import inv from './img/invitational.jpg'
+import bf from './img/bf.png'
+import apex_wide from './img/apex_wide.png'
+import ow_wide from './img/ow_wide.png'
+import fort from './img/fort.jpg'
+import lol from './img/lol.jpg'
+import pubg from './img/pubg.png'
+import BL3 from './img/BL3.png'
+import BF1 from './img/BF1.png'
+import BF4 from './img/BF4.png'
+import rocket from './img/rocket.png'
+import R6_D from './img/r6dark.jpg'
+import DST from './img/destiny.png'
+import DST2 from './img/destiny2.png'
 
 const alex = function () {
   alert('alex');
@@ -12,15 +28,16 @@ const alex = function () {
 function Discover() {
   return (
     <div class='container'>
-    <main >
-      <div class='row'>{<GameCard logo={OW}/>}{<GameCard logo={R6}/>}{<GameCard logo={OW} />}</div>
-      <div class='row'>{<GameCard logo={R6}/>}{<GameCard logo={dota}/>}{<GameCard logo={R6}/>}</div>
-      <div class='row'>{<GameCard logo={OW}/>}{<GameCard logo={R6}/>}{<GameCard logo={OW}/>}</div>
-      <div class='row'>{<GameCard logo={R6}/>}{<GameCard logo={dota}/>}{<GameCard logo={R6}/>}</div>
-      <div class='row'>{<GameCard logo={OW}/>}{<GameCard logo={R6}/>}{<GameCard logo={OW}/>}</div>
-      <div class='row'>{<GameCard logo={R6}/>}{<GameCard logo={dota}/>}{<GameCard logo={R6}/>}</div>
-      <div class='row'>{<GameCard logo={OW}/>}{<GameCard logo={R6}/>}{<GameCard logo={OW}/>}</div>
-    </main>
+      <nav>
+        <div>GAMETRAX</div>
+        <div>PAROGIE</div>
+        <div>BROROGIE</div>
+        <div>JOEROGI</div>
+      </nav>
+      <row1>{<GameCard logo={fort}/>}{<GameCard logo={pubg}/>}{<GameCard logo={DST2}/>}{<GameCard logo={fort}/>}</row1>
+      <row2>{<GameCard logo={BL3}/>}{<GameCard logo={apex}/>}{<GameCard logo={dota}/>}{<GameCard logo={BL3}/>}</row2>
+      <row3>{<GameCard logo={R6_D}/>}{<GameCard logo={OW}/>}{<GameCard logo={BF1}/>}{<GameCard logo={R6_D}/>}</row3>
+      <row4>{<GameCard logo={OW}/>}{<GameCard logo={R6}/>}{<GameCard logo={R6}/>}{<GameCard logo={OW}/>}</row4>
     </div>
   );
 }
--- src/GameCard.js
+++ src/GameCard.js
@@ -4,7 +4,7 @@ import './GameCard.css';
 function GameCard(props) {
   return (
     <body class='title_card' onClick={() => alert(props.logo)}>
-      <img class='ow'src={props.logo} className="App-logo" alt="logo" />
+      <img id='ow'src={props.logo} alt="logo" />
     </body>
   );
 }
