9da75d932d2074334eac38128c9489941a1c1d97
QuintonEL <quintonlaborde@gmail.com>
2020-03-14 20:33:08 -0600

    added gamecards onto profile page with carousel effect and fixed some banner styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Sidebar from './components/Sidebar'
 import Banner from './components/Banner'
+import GameCards from './components/GameCards'
 import './App.css';
 import './components/Sidebar.css'
 import './components/Banner.css'
+import './components/GameCards.css'
 
 
 function App() {
   return (
     <div>
       <Banner/>
+      <GameCards/>
       <Sidebar/>
     </div>
   )
---
+++ src/components/GameCards.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function GameCards() {
+
+  return (
+    <div>
+    <div class="container">
+      <div class="item"><img src='https://www.mobygames.com/images/covers/l/591676-borderlands-3-playstation-4-front-cover.jpg' alt='pic'/></div>
+      <div class="item"><img src='https://images-na.ssl-images-amazon.com/images/I/51M0IBliDDL._AC_.jpg' alt='pic'/></div>
+      <div class="item"><img src='https://i.pinimg.com/originals/25/ce/55/25ce55dc0c0415ed4d02871b1f27b270.jpg' alt='pic'/></div>
+      <div class="item"><img src='https://static-cdn.jtvnw.net/ttv-boxart/Total%20War%3A%20Warhammer%20II.jpg' alt='pic'/></div>
+    </div>
+    {/* <div class="content">
+    <div class="background">
+      <div class="left">left</div>
+      <div class="right">right</div>
+    </div>
+    <div class="content-container">content here...</div>
+  </div> */}
+  </div>
+  )
+
+}
+
+export default GameCards
+
