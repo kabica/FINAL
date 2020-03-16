4627a58766d1dbeb9148c8431e504f98f321cb5d
Kevin Alexander <kalex.bica@gmail.com>
2020-03-16 12:28:21 -0600

    fully impliment game description drop down

--- src/Discover.js
+++ src/Discover.js
@@ -4,60 +4,91 @@ import GameCard from './GameCard';
 import OW from './img/ow.jpg';
 import R6 from './img/r6.jpg'
 import dota from './img/dota.jpg'
-import R6D from './img/r6dark.jpg';
 import apex from './img/apex.jpeg';
-import inv from './img/invitational.jpg'
-import bf from './img/bf.png'
-import apex_wide from './img/apex_wide.png'
-import ow_wide from './img/ow_wide.png'
 import fort from './img/fort.jpg'
-import lol from './img/lol.jpg'
 import pubg from './img/pubg.png'
 import BL3 from './img/BL3.png'
 import BF1 from './img/BF1.png'
-import BF4 from './img/BF4.png'
-import rocket from './img/rocket.png'
 import R6_D from './img/r6dark.jpg'
-import DST from './img/destiny.png'
 import DST2 from './img/destiny2.png'
 import Show from './Show'
 import { useState } from "react";
 
 const text = {
-  fort: 'A COOL GAME FOR COOL PEOPLE BUT PEOPLE WITH SMALL PEROGIES',
+  FORT: 'A COOL GAME FOR COOL PEOPLE BUT PEOPLE WITH SMALL PEROGIES',
   PUBG: 'a game where people run around and suddenly get slammed by a dude in a jeep and then all the fun you were once having is over',
-  paula: 'Paula is a cool mom!'
+  paula: 'Paula is a cool mom!',
+  BL3: 'borderlands3',
+  APEX: 'Apex legends',
+  DOTA: 'dota',
+  R6: 'rainbow six',
+  OW: 'overwatch',
+  BF1: 'battlefield1'
 }
 
 function Discover() {
-  const [state, setState] = useState('hide');
-  const alex = function(ID) {
-    setState(text[ID]);
+  const initial = {
+    row1: 'hide_R1',
+    row2: 'hide_R2',
+    row3: 'hide_R3',
+    row4: 'hide_R4'
   }
-  const bica = function(ID) {
-    setState('hide');
+  const [mode, setMode] = useState(initial);
+  const setVisual = function(row, ID) {
+    if(row === 'row1') setMode({...mode, row1: text[ID]})
+    if(row === 'row2') setMode({...mode, row2: text[ID]})
+    if(row === 'row3') setMode({...mode, row3: text[ID]})
+    if(row === 'row4') setMode({...mode, row4: text[ID]})
+  };
+  const hideVisual = function(row) {
+    if(row === 'row1') setMode({...mode, row1: 'hide_R1'})
+    if(row === 'row2') setMode({...mode, row2: 'hide_R2'})
+    if(row === 'row3') setMode({...mode, row3: 'hide_R3'})
+    if(row === 'row4') setMode({...mode, row4: 'hide_R4'})
   }
+
   return (
-    <div class='container'>
+    <main class='container'>
       <nav>
         <div>GAMETRAX</div>
         <div>PAROGIE</div>
         <div>BROROGIE</div>
         <div>JOEROGI</div>
       </nav>
-      <main>
-        <row1>
-          {<GameCard logo={fort} onClick={() => alex('fort')}/>}
-          {<GameCard logo={pubg} onClick={() => alex('PUBG')}/>}
-          {<GameCard logo={DST2} onClick={() => alex('paula')}/>}
-          {<GameCard logo={fort}/>}
-        </row1>
-        {state !== 'hide' && (<Show name={state} onClick={bica}/>)}
-        <row2>{<GameCard logo={BL3}/>}{<GameCard logo={apex}/>}{<GameCard logo={dota}/>}{<GameCard logo={BL3}/>}</row2>
-        <row3>{<GameCard logo={R6_D}/>}{<GameCard logo={OW}/>}{<GameCard logo={BF1}/>}{<GameCard logo={R6_D}/>}</row3>
-        <row4>{<GameCard logo={OW}/>}{<GameCard logo={R6}/>}{<GameCard logo={R6}/>}{<GameCard logo={OW}/>}</row4>
-      </main>
-    </div>
+      <body>
+        <div id='row1'>
+          {<GameCard logo={fort} onClick={() => setVisual('row1','FORT')}/>}
+          {<GameCard logo={pubg} onClick={() => setVisual('row1','PUBG')}/>}
+          {<GameCard logo={DST2} onClick={() => setVisual('row1','paula')}/>}
+          {<GameCard logo={fort} onClick={() => setVisual('row1','FORT')}/>}
+        </div>
+        {mode['row1'] !== 'hide_R1' && (<Show name={mode['row1']} onClick={() => hideVisual('row1')}/>)}
+
+        <div id='row2'>
+          {<GameCard logo={BL3} onClick={() => setVisual('row2','BL3')}/>}
+          {<GameCard logo={apex} onClick={() => setVisual('row2','APEX')}/>}
+          {<GameCard logo={dota} onClick={() => setVisual('row2','DOTA')}/>}
+          {<GameCard logo={BL3} onClick={() => setVisual('row2','BL3')}/>}
+        </div>
+        {mode['row2'] !== 'hide_R2' && (<Show name={mode['row2']} onClick={() => hideVisual('row2')}/>)}
+
+        <div id='row3'>
+          {<GameCard logo={R6_D} onClick={() => setVisual('row3','R6')}/>}
+          {<GameCard logo={OW} onClick={() => setVisual('row3','OW')}/>}
+          {<GameCard logo={BF1} onClick={() => setVisual('row3','BF1')}/>}
+          {<GameCard logo={R6_D} onClick={() => setVisual('row3','R6')}/>}
+        </div>
+        {mode['row3'] !== 'hide_R3' && (<Show name={mode['row3']} onClick={() => hideVisual('row3')}/>)}
+
+        <div id='row4'>
+          {<GameCard logo={OW} onClick={() => setVisual('row4','OW')}/>}
+          {<GameCard logo={R6} onClick={() => setVisual('row4','R6')}/>}
+          {<GameCard logo={R6} onClick={() => setVisual('row4','R6')}/>}
+          {<GameCard logo={OW} onClick={() => setVisual('row4','OW')}/>}
+        </div>
+        {mode['row4'] !== 'hide_R4' && (<Show name={mode['row4']} onClick={() => hideVisual('row4')}/>)}
+      </body>
+    </main>
   );
 }
 
