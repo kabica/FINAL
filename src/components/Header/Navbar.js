454b3d71bb0e6ac3207e4092b45b839992fad5d9
QuintonEL <quintonlaborde@gmail.com>
2020-03-17 20:34:41 -0600

    uploaded images of the new logo, implemented them in the navbar

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react"
 import "./Navbar.css"
-import avatar from '../../components/public/images/chief.jpg'
+import logo from '../public/images/gametrax-g.png'
+import name from '../public/images/gametrax-letters.png'
 
 
 const Navbar = ({ sticky }) => (
   <nav className={sticky ? "navbar navbar-sticky" : "navbar"}>
     <div className="navbar--logo-holder">
-      {sticky ? <img src={avatar} alt="avatar" className="navbar--logo" /> : null}
-      <h1> GameTrax</h1>
+      {sticky ? <img src={logo} alt="avatar" className="navbar--logo" /> : null}
+      {sticky? null : <img src={name} alt="name" id="name"/>}
     </div>
     <ul className="navbar--link">
       <li className="navbar--link-item"><a href='#profile'>Username</a></li>
