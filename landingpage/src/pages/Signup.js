724dca0ea948dee5c68c9e26ec3891582c3fb269
Mohamed <mbchehade@gmail.com>
2020-03-18 15:54:39 -0600

    added a background to sign up page

--- landingpage/src/pages/Signup.js
+++ landingpage/src/pages/Signup.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-
 import SignupForm from '../components/signup/SignupForm';
 import SignupFooter from '../components/signup/SignupFooter';
+import classes from './signup.module.css'
 
 class Signup extends Component {
   render() {
     return (
-      <div className="main-singup-container">
+      <div>
+      <div className={classes.Background}>
         <div className="header-top">
         </div>
         <SignupForm />
+        </div>
         <SignupFooter />
       </div>
     );
