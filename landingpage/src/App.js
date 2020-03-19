945a7365b713d41011daea619a795a4a0f48db46
Mohamed <mbchehade@gmail.com>
2020-03-19 10:02:08 -0600

    deleted an unused route

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import './css/App.css';
 import Main from './pages';
 import Login from './pages/Login';
 import Signup from './pages/Signup'
 
 import { Switch, Route } from 'react-router-dom';
-import ChoosePlan from './pages/ChoosePlan';
 
 
 function App() {
@@ -14,7 +12,6 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/login" component={Login} />
-        <Route path="/choose-plan" component={ChoosePlan} />
         <Route path="/signup" component={Signup}/>
       </Switch>
     </div>
