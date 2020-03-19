9ee74064036a0a2d4efab430af48c6fae031d97f
Mohamed <mbchehade@gmail.com>
2020-03-19 11:49:45 -0600

    preparing to merge

--- landingpage/src/components/Header.js
+++ landingpage/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { Button } from './Button';
-import Icon from 'react-icons-kit';
+
 
 import BackgroundVideo from "./BackgroundVideo"
 
