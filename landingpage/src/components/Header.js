ea4f6195b7120b605f991c7ac18544a8ef1c2d16
Mohamed <mbchehade@gmail.com>
2020-03-19 09:57:29 -0600

    created a component that for the header of the page which takes background video as a tag to render it

---
+++ landingpage/src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { Button } from './Button';
+import Icon from 'react-icons-kit';
+
+import BackgroundVideo from "./BackgroundVideo"
+
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header-top">
+          <BackgroundVideo/>
+        </div>
+        <div className="header-content">
+          <Link to="/login">
+            <Button className="main-offer-btn" primary>
+              try it now
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
+
+
