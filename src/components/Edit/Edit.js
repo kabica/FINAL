02488ae71897a4b982b1a5f1bf291ae7cbcdb0f7
Kevin Alexander <kalex.bica@gmail.com>
2020-03-21 19:02:50 -0600

    refactor profile edit page and avatar update component

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,45 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Edit.css';
 import { useState } from "react";
 import YouTube from 'react-youtube';
 import SpotifyPlayer from 'react-spotify-player';
 import DISCORD from '../img/discord.png'
 import AliasCard from './AliasCard'
+import { v } from './editConfig';
+import axios from 'axios';
+import CHEF from '../img/halo.png'
+import STAR from '../img/star.png'
 
 const onReady = function(event) {
   // access to player in all event handlers via event.target
   event.target.pauseVideo();
 }
-const opts = {
-  width: '260',
-  height: '180',
-}
-const size = {
-  width: '100%',
-  height: 200,
-};
-const view = 'list'; // or 'coverart'
-const theme = 'black'; // or 'white'
+
+
 
 function Edit(props) {
-
   const initial = {
+    MODE: 'done',
     YT1: props.YT1 || 'Enter a video URL',
     YT2: props.YT2 || 'Enter a video URL',
     YT3: props.YT3 || 'Enter a video URL',
     SPOT1: props.SPOT1 || 'Enter a playlist URL',
     SPOT2: props.SPOT2 || 'Enter a playlist URL',
-    SPOT3: props.SPOT3
-  }
-  const [state, setState] = useState(initial)
+    SPOT3: props.SPOT3 || 'Enter a playlist URL',
+    avatar: CHEF,
+    BANNER: props.BANNER || 'Enter a banner image URL'
+  };
+  const [state, setState] = useState({initial});
+  const updateProfile = function(text) {
+    setState({...state, MODE: 'saving'})
+    return axios.post('http://localhost:8000/alex', {
+      video1: state.YT1 || '',
+      video2: state.YT2 || '',
+      video3: state.YT3 || '',
+      song1: state.SPOT1 || '',
+      song2: state.SPOT2 || '',
+      song3: state.SPOT3 || '',
+      user: 1
+    })
+    .then(() => setState({...state, MODE: 'done'}))
+    .catch(err => console.log(err))
+  };
+  useEffect(() => {
+    setState({...state, avatar: STAR})
+  },[]);
  
   return (
     <div id='edit_page'>
       <div class='page_header'>Profile Edit</div>
       
-      <div class='category_header'>Avatar</div>
+      <div class='category_header'>Avatar + Banner</div>
       <div id='avatar_card'>
-        <img src={state.avatar} alt=''></img>
+        <img id='current_img'src={state.avatar} alt=''></img>
+        <div id='profile_options'>
+          <div id='avatar_options'>
+            <img id='avatar_img'src={CHEF} alt='' onClick={() => setState({...state, avatar: CHEF})}></img>
+            <img id='avatar_img'src={CHEF} alt=''></img>
+            <img id='avatar_img'src={CHEF} alt=''></img>
+            <img id='avatar_img'src={CHEF} alt=''></img>
+            <img id='avatar_img'src={CHEF} alt=''></img>
+            <img id='avatar_img'src={CHEF} alt=''></img>
+          </div>
+          <div id='banner_select'>
+            <div class='banner_header'>Banner URL:</div>
+            <input
+              value={state.BANNER || ''}
+              placeholder='Enter a banner image URL'
+              onChange={(event) => {setState({...state, BANNER: event.target.value})}}
+              type="text"
+            />
+          </div>
+        </div>
+        
       </div>
 
       <div class='category_header'>Platform Info</div>
@@ -49,7 +84,7 @@ function Edit(props) {
       <div id='youtube_card'>
         <div id='videos'>
           <div id='video_col'>
-            <YouTube id='vid'videoId="M7_s_mvC0O8"onReady={onReady}opts={opts}/>
+            <YouTube id='vid'videoId="M7_s_mvC0O8"onReady={onReady}opts={v.opts}/>
             <input 
               placeholder='Enter a video URL'
               value={state.YT1 || ""}
@@ -58,7 +93,7 @@ function Edit(props) {
             />
           </div>
           <div id='video_col'>
-            <YouTube id='vid'videoId="M7_s_mvC0O8"onReady={onReady}opts={opts}/>
+            <YouTube id='vid'videoId="M7_s_mvC0O8"onReady={onReady}opts={v.opts}/>
             <input 
               placeholder='Enter a video URL'
               value={state.YT2 || ""}
@@ -67,9 +102,8 @@ function Edit(props) {
             />
           </div>
           <div id='video_col'>
-            <YouTube id='vid'videoId="M7_s_mvC0O8"onReady={onReady}opts={opts}/>
+            <YouTube id='vid'videoId="M7_s_mvC0O8"onReady={onReady}opts={v.opts}/>
             <input 
-              placeholder='Enter a video URL'
               value={state.YT3 || ""}
               onChange={(event) => {setState({...state, YT3: event.target.value})}}
               type="text"
@@ -84,9 +118,9 @@ function Edit(props) {
             <div id='video_col'>
             <SpotifyPlayer
               uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
-              size={size}
-              view={view}
-              theme={theme}
+              size={v.size}
+              view={v.view}
+              theme={v.theme}
             />
             <input 
               placeholder='Enter a playlist URL'
@@ -98,9 +132,9 @@ function Edit(props) {
           <div id='video_col'>
             <SpotifyPlayer
               uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
-              size={size}
-              view={view}
-              theme={theme}
+              size={v.size}
+              view={v.view}
+              theme={v.theme}
             />
             <input 
               placeholder='Enter a playlist URL'
@@ -112,9 +146,9 @@ function Edit(props) {
           <div id='video_col'>
             <SpotifyPlayer
               uri="spotify:album:1TIUsv8qmYLpBEhvmBmyBk"
-              size={size}
-              view={view}
-              theme={theme}
+              size={v.size}
+              view={v.view}
+              theme={v.theme}
             />
             <input 
               placeholder='Enter a playlist URL'
@@ -124,6 +158,12 @@ function Edit(props) {
             />
           </div>
         </div>
+      </div>
+      <div id='confirm_button'>
+        <a href='/profile'>
+        <button onClick={updateProfile}>Confirm</button>
+        </a>
+        
       </div>
     </div>
   );
---
+++ src/components/Edit/editConfig.js
@@ -0,0 +1,15 @@
+const v = {
+  opts : {
+    width: '260',
+    height: '180',
+  },
+  size : {
+    width: '100%',
+    height: 200,
+  },
+  view : 'list',
+  theme : 'black',
+
+}
+
+module.exports = { v };
