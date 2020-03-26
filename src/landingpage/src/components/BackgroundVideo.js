2c181e51906f652b8e52f54cb5832436b405d30d
Mohamed <mbchehade@gmail.com>
2020-03-25 21:24:07 -0600

    merging

--- src/landingpage/src/components/BackgroundVideo.js
+++ src/landingpage/src/components/BackgroundVideo.js
@@ -2,29 +2,33 @@ import React from 'react';
 
 import classes from './BackgroundVideo.module.css';
 import styled from 'styled-components';
-// import { generateMedia } from 'styled-media-query';
+import { generateMedia } from 'styled-media-query';
 import video from '../Videos/main1.mp4'
 import images from '../images/logo.png'
-import { NavLink, Link } from 'react-router-dom';
-
+// import { NavLink, Link } from 'react-router-dom';
+import pc from '../images/pc.png'
+import ps4 from '../images/ps4.png'
+import nintendo from '../images/nintendo.png'
+import xbox from '../images/xbox.png';
+import './BackgroundVideo.module.css';
 
 
 
 
 const BackgroundVideo = () => {
 
-  return (
-      
+    return (
+
         <div className={classes.Container} >
-        
+
             <video autoPlay="autoplay" loop="loop" muted className={classes.Video} >
                 <source src={video} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className={classes.Content}>
                 <div className={classes.SubContent} >
-            <img src={images} alt="gametrax logo"/>
-                <SubTitle>Connect with Everyone. Track Anyone</SubTitle>
+                    <img src={images} alt="gametrax logo" />
+                    <SubTitle>Connect with Everyone. Track Anyone</SubTitle>
 
                     <a href="/signup">
                         <button type="button" className="btn btn-outline-dark">Signup Now!</button>
@@ -33,20 +37,26 @@ const BackgroundVideo = () => {
                     <a href="/login">
                         <button type="button" className="btn btn-outline-dark">Sign In</button>
                     </a>
-            
+
                 </div>
+                <span className={classes.icons_moe}>
+                    <img className="pc" src={pc} alt="pc" />
+                    <img className="nintendo" src={nintendo} alt="nintendo" />
+                    <img className="ps4" src={ps4} alt="ps4" />
+                    <img className="xbox" src={xbox} alt="xbox" />
+                </span>
             </div>
         </div>
     )
 }
 
-// // Media
-// const customMedia = generateMedia({
-//   lgDesktop: '1350px',
-//   mdDesktop: '1150px',
-//   tablet: '960px',
-//   smTablet: '740px'
-// });
+// Media
+const customMedia = generateMedia({
+    lgDesktop: '1350px',
+    mdDesktop: '1150px',
+    tablet: '960px',
+    smTablet: '740px'
+});
 
 
 // SubTitle
--- src/landingpage/src/components/Header.js
+++ src/landingpage/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Button } from './Button';
+// import { Button } from './Button';
 
 
 import BackgroundVideo from "./BackgroundVideo"
@@ -12,20 +12,14 @@ class Header extends Component {
     return (
       <div className="header-container">
         <div className="header-top">
-          <BackgroundVideo/>
+          <BackgroundVideo />
         </div>
-        <div className="header-content">
-          <Link to="/login">
-            <Button className="main-offer-btn" primary>
-              try it now
-            </Button>
-          </Link>
-        </div>
+        {/* <div className="header-content">
+          
+        </div> */}
       </div>
     );
   }
 }
 
 export default Header;
-
-
