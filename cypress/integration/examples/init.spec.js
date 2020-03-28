7440130f2b2a2d828f268d3b4b362ba6e83ba35e
QuintonEL <quintonlaborde@gmail.com>
2020-03-28 15:06:57 -0600

    moved a test around

--- cypress/integration/examples/init.spec.js
+++ cypress/integration/examples/init.spec.js
@@ -2,8 +2,7 @@ describe('Cypress', () => {
   it('is working', () => {
     expect(true).to.equal(true)
   })
-})
-
-it('visits the app', () => {
-  cy.visit('/')
+  it('visits the app', () => {
+    cy.visit('/')
+  })
 })
